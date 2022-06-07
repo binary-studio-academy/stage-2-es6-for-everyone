9f46b503df784cc94cf52067e73c731903349ef6
Revenets <revenets@gmail.com>
2022-06-07 17:22:14 +0300

    Add getFighterInfo func logic

--- src/javascript/components/fighterSelector.js
+++ src/javascript/components/fighterSelector.js
@@ -2,9 +2,11 @@ import { createElement } from '../helpers/domHelper';
 import { renderArena } from './arena';
 import versusImg from '../../../resources/versus.png';
 import { createFighterPreview } from './fighterPreview';
+import { fighterService } from '../services/fightersService';
 
 export function createFightersSelector() {
   let selectedFighters = [];
+  // console.log(selectedFighters)
 
   return async (event, fighterId) => {
     const fighter = await getFighterInfo(fighterId);
@@ -21,6 +23,13 @@ const fighterDetailsMap = new Map();
 
 export async function getFighterInfo(fighterId) {
   // get fighter info from fighterDetailsMap or from service and write it to fighterDetailsMap
+  if (fighterDetailsMap.has(fighterId)) {
+    return fighterDetailsMap.get(fighterId);
+  } else {
+    let fighter = await fighterService.getFighterDetails(fighterId);
+    fighterDetailsMap.set(fighterId, fighter);
+    return fighter;
+  }
 }
 
 function renderSelectedFighters(selectedFighters) {
