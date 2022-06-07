bb677a880a6db445e839d6b01d4da19232f33012
Revenets <revenets@gmail.com>
2022-06-07 17:57:34 +0300

    add show winner modal logic

--- src/javascript/components/modal/winner.js
+++ src/javascript/components/modal/winner.js
@@ -1,3 +1,8 @@
+import { showModal } from "./modal";
+
 export function showWinnerModal(fighter) {
   // call showModal function 
+  const title = "Game over"
+  const bodyElement = `${fighter.name} wins!` 
+  showModal({title, bodyElement})
 }
